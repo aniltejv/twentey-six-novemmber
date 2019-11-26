906ea566bf8a668386ca8fff194527db5f1ca33f
aniltejv <aniltej123456.at@gmail.com>
2019-11-26 10:57:49 +0530

    mnk

---
+++ ahe.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+	char acdata[5]={'a','b','c','d'};
+	char *pudata=NULL;
+	int i;
+//	for(i=0;i<5;i++)
+//	{
+	acdata[5]=pudata;
+		printf("%c",*pudata++);
+//	}
+}
+
